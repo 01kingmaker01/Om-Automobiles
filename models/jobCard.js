011c987ba9445aed5f70288f835f454d7913d38e
Ketan Chavan <www.ketanchavan2600@gmail.com>
2022-12-20 05:13:52 +0530

    search bar added in openJobCard

--- models/jobCard.js
+++ models/jobCard.js
@@ -1,34 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-
-
-const jobCardScehma = new mongoose.Schema(
-    {
-        vehicleNumber:{
-            type:String
-        },
-        customerName:{
-            type:String
-        },
-        vehicleModel:{
-            type:String
-        },
-        contactNumber:{
-            type:Number
-        },
-        openJobCard:{
-          type: Number
-        },
-        completeJobCard:{
-            type: Number
-        },
-        closeJobCard:{
-            type: Number
-        }
-        // kM:{
-        //     type:Number
-        // }
-    }
-);
-module.exports = mongoose.model('JobCard' , jobCardScehma);
+const jobCardScehma = new mongoose.Schema({
+  vehicleNumber: {
+    type: String,
+  },
+  customerName: {
+    type: String,
+  },
+  vehicleModel: {
+    type: String,
+  },
+  contactNumber: {
+    type: Number,
+  },
+  openJobCard: {
+    type: Number,
+  },
+  completeJobCard: {
+    type: Number,
+  },
+  closeJobCard: {
+    type: Number,
+  },
+  // kM:{
+  //     type:Number
+  // }
+});
+module.exports = mongoose.model("JobCard", jobCardScehma);
--- routes/closeJobCard.js
+++ routes/closeJobCard.js
@@ -5,35 +5,27 @@ const JobCard = require("../models/jobCard");
 const JobBill = require("../models/jobBill");
 const app = express();
 
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.json());
 
-
 //test
 
-let path = require('path');
-let filePath = "./views/closeJobCard.html"
+let path = require("path");
+let filePath = "./views/closeJobCard.html";
 let resolvedPath = path.resolve(filePath);
 
-
-
 router
-   .route("/")
-   .get(async (req,res)=>{
+  .route("/")
+  .get(async (req, res) => {
     //console.log(__dirname+'/views/closeJobCard.html');
-    let cards = await JobCard.find({closeJobCard : 1});
+    let cards = await JobCard.find({ closeJobCard: 1 });
     console.log(cards);
-    res.render("closeJobCard", {closeJobCard : cards});
-   })
-   .post((req,res)=>{
-       res.redirect("/");
-   });
-router
-   .route("/getCards")
-   .post(async (req,res)=>{
-      
-   })
+    res.render("closeJobCard", { closeJobCard: cards });
+  })
+  .post((req, res) => {
+    res.redirect("/");
+  });
+router.route("/getCards").post(async (req, res) => {});
 
 module.exports = router;
