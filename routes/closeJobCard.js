d364111c98c402fbfa57b2d5a903659ac09ff3ca
piyush-15 <piyushpatil824@gmail.com>
2022-12-25 16:26:21 +0530

    h

--- routes/closeJobCard.js
+++ routes/closeJobCard.js
@@ -5,35 +5,73 @@ const JobCard = require("../models/jobCard");
 const JobBill = require("../models/jobBill");
 const app = express();
 
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.json());
 
-
 //test
 
-let path = require('path');
-let filePath = "./views/closeJobCard.html"
+let path = require("path");
+let filePath = "./views/closeJobCard.html";
 let resolvedPath = path.resolve(filePath);
 
-
-
 router
-   .route("/")
-   .get(async (req,res)=>{
-    //console.log(__dirname+'/views/closeJobCard.html');
-    let cards = await JobCard.find({closeJobCard : 1});
-    console.log(cards);
-    res.render("closeJobCard", {closeJobCard : cards});
-   })
-   .post((req,res)=>{
-       res.redirect("/");
-   });
-router
-   .route("/getCards")
-   .post(async (req,res)=>{
-      
-   })
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      //For Search Bar
+
+      let input;
+      let noMatch = false;
+
+      //regrex fuction for Fuzzy Logic
+      //Link => https://stackoverflow.com/questions/38421664/fuzzy-searching-with-mongodb
+
+      function escapeRegex(text) {
+        return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+      }
+
+      console.log(req.query.search);
+
+      if (req.query.search) {
+        input = req.query.search;
+        const regex = new RegExp(escapeRegex(input), "gi");
+
+        const closeJobCards = await JobCard.find({
+          closeJobCard: 1,
+          customerName: regex,
+        });
+        //https://kb.objectrocket.com/mongo-db/or-in-mongoose-1018
+
+        //Check whether Cards array is empty
+        if (closeJobCards.length < 1) {
+          noMatch = true;
+        }
+
+        return res.render("closeJobCard", {
+          input,
+          noMatch,
+          closeJobCards,
+        });
+      } else {
+        //original Code
+        let cards = await JobCard.find({ closeJobCard: 1 });
+
+        //Only input , noMatch added
+        return res.render("closeJobCard", {
+          closeJobCards: cards,
+          input,
+          noMatch,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  })
+
+  .post((req, res) => {
+    res.redirect("/");
+  });
+router.route("/getCards").post(async (req, res) => {});
 
 module.exports = router;
--- routes/completeJobCard.js
+++ routes/completeJobCard.js
@@ -6,90 +6,111 @@ const JobBill = require("../models/jobBill");
 const TotalRevenue = require("../models/totalRevenue");
 const { findOne } = require("../models/jobCard");
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.json());
 
-
 let utc = new Date();
-utc.setHours( utc.getHours() + 5);
-utc.setMinutes( utc.getMinutes() + 30);
+utc.setHours(utc.getHours() + 5);
+utc.setMinutes(utc.getMinutes() + 30);
 //domain.updated = utc;
 
-
-
 router
-   .route("/")
-   .get(async (req,res)=>{
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      //For Search Bar
 
+      let input;
+      let noMatch = false;
 
+      //regrex fuction for Fuzzy Logic
+      //Link => https://stackoverflow.com/questions/38421664/fuzzy-searching-with-mongodb
 
+      function escapeRegex(text) {
+        return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+      }
 
-      //test
-      const bill = new JobBill({
-         customerName:"Piyush", 
-         billDate:utc
-      });
+      console.log(req.query.search);
 
-      console.log(bill.billDate.getDate());
-      // bill.save();
+      if (req.query.search) {
+        input = req.query.search;
+        const regex = new RegExp(escapeRegex(input), "gi");
 
+        const completeJobCards = await JobCard.find({
+          completeJobCard: 1,
+          customerName: regex,
+        });
+        //https://kb.objectrocket.com/mongo-db/or-in-mongoose-1018
 
+        //Check whether Cards array is empty
+        if (completeJobCards.length < 1) {
+          noMatch = true;
+        }
 
-      
-      let cards = await JobCard.find({completeJobCard : 1});
-      
-      
-      res.render("completeJobCard", {completeJobCards : cards});
-   })
-   .post((req,res)=>{
-      res.redirect("/");
-   });
+        return res.render("completeJobCard", {
+          input,
+          noMatch,
+          completeJobCards,
+        });
+      } else {
+        //original Code
+        let cards = await JobCard.find({ completeJobCard: 1 });
+
+        //Only input , noMatch added
+        return res.render("completeJobCard", {
+          completeJobCards: cards,
+          input,
+          noMatch,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  })
+
+  .post((req, res) => {
+    res.redirect("/");
+  });
 router
-   .route("/:cardID")
-   .get(async (req,res)=>{
-      const cardid = {_id: req.params.cardID};
-      let card =await JobCard.findOne({cardid , completeJobCard : 1});
-      res.render("makeBill", {card : card});
+  .route("/:cardID")
+  .get(async (req, res) => {
+    const cardid = { _id: req.params.cardID };
+    let card = await JobCard.findOne({ cardid, completeJobCard: 1 });
+    res.render("makeBill", { card: card });
+  })
+  .post(async (req, res) => {
+    let currOil = parseInt(req.body.oilPrice);
 
-   })
-   .post(async (req,res)=>{
-      let currOil = parseInt(req.body.oilPrice);
-      
-      let currLub = req.body.lubPrice;
-      let currLabour = req.body.labourPrice;
-      let currPart = req.body.partPrice;
-      TotalRevenue.findOne({name:'Oil'} , function(err , curr){
-         curr.total = curr.total + currOil;
-         curr.save(); 
-      });
+    let currLub = req.body.lubPrice;
+    let currLabour = req.body.labourPrice;
+    let currPart = req.body.partPrice;
+    TotalRevenue.findOne({ name: "Oil" }, function (err, curr) {
+      curr.total = curr.total + currOil;
+      curr.save();
+    });
 
-     
+    const cardid = { _id: req.params.cardID };
+    const update = { completeJobCard: 0, closeJobCard: 1 };
+    let card = await JobCard.findOneAndUpdate(cardid, update);
+    const bill = new JobBill({
+      vehicleNumber: card.carNumber,
+      customerName: card.customerName,
+      vehicleModel: card.vehicleModel,
+      contactNumber: card.contactNumber,
+      oilDesc: req.body.oilDesc,
+      oilCharges: req.body.oilPrice,
+      labourDesc: req.body.labourDesc,
+      labourCharges: req.body.labourPrice,
+      sparePartDesc: req.body.partDesc,
+      sparePartCharges: req.body.partPrice,
+      lubDesc: req.body.lubDesc,
+      lubCharges: req.body.lubPrice,
+    });
 
+    bill.save();
 
-
-      const cardid = {_id: req.params.cardID};
-      const update = {completeJobCard : 0 , closeJobCard:1};
-      let card = await JobCard.findOneAndUpdate(cardid , update);
-      const bill = new JobBill({
-         vehicleNumber:card.carNumber,
-         customerName:card.customerName,
-         vehicleModel:card.vehicleModel,
-         contactNumber:card.contactNumber,
-         oilDesc:req.body.oilDesc,
-        oilCharges:req.body.oilPrice,
-        labourDesc:req.body.labourDesc,
-        labourCharges:req.body.labourPrice,
-        sparePartDesc:req.body.partDesc,
-        sparePartCharges:req.body.partPrice,
-        lubDesc:req.body.lubDesc,
-        lubCharges:req.body.lubPrice
-     });
-
-     bill.save();
-    
-      
-      res.redirect("/closeJobCard");
-   })
+    res.redirect("/closeJobCard");
+  });
 
 module.exports = router;
